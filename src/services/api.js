233c304e22ff90aedeb3898fccdec4376ab8bc60
jacksrm <62717829+jacksrm@users.noreply.github.com>
2020-10-17 14:28:55 -0300

    adicionado variaveis de ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({ 
-  baseURL: 'https://pergen-2-backend.herokuapp.com/'
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
